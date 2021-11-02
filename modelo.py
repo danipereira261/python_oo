2f0451bcb28ea2fa64855b0ed2feb0d2e8e1ca90
danipereira261 <dpereira261@gmail.com>
2021-11-01 21:48:55 -0300

    Adicionando atributos e métodos

--- modelo.py
+++ modelo.py
@@ -1,18 +1,35 @@
 class Filme:
     def __init__(self, nome, ano, duracao):
-        self.nome = nome
+        self.nome = nome.title()
         self.ano = ano
         self.duracao = duracao
+        self.likes = 0
+
+    def dar_like(self):
+        self.likes += 1
+
+
+
 
 class Serie:
     def __init__(self, nome, ano, temporadas):
-        self.nome = nome
+        self.nome = nome.title()
         self.ano = ano
         self.temporadas = temporadas
+        self.likes = 0
+
+    def dar_like(self):
+        self.likes += 1
 
 
-vingadores = Filme('Vingadores - Guerra Infinita', 2018, 160)
-print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao}')
+vingadores = Filme('vingadores - guerra infinita', 2018, 160)
+vingadores.dar_like()
 
-atlanta = Serie('Atlanta', 2017, 2)
-print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} - Temporadas: {atlanta.temporadas}')
+print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao} - Likes: {vingadores.likes}')
+
+
+atlanta = Serie('atlanta', 2017, 2)
+atlanta.dar_like()
+atlanta.dar_like()
+
+print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} - Temporadas: {atlanta.temporadas} - Likes: {atlanta.likes}')
